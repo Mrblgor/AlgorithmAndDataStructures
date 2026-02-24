a348e5d8605b4e979bc392e89ef6bdde1dcb8240
Mrblgor <igor.bolotov.19@bk.ru>
2026-02-24 13:48:47 +0700

    Update Program.cs

--- GuessNumber/Program.cs
+++ GuessNumber/Program.cs
@@ -1,56 +1,89 @@
-﻿using System.Diagnostics.Metrics;
-namespace GuessNumber
+using System.Diagnostics.Metrics; // Подключение пространства имён (в данном коде не используется)
+
+namespace GuessNumber // Объявление пространства имён
 {
-    internal class Program
+    internal class Program // Главный класс программы
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // Точка входа в программу
         {
-            int min = 0;
-            int max = 0;
-            int count = 0;
-            int countGame = 0;
-            Random rnd = new Random();
-            char answer = 'Y';
-            do
+            int min = 0; // Минимальное количество попыток за все игры
+            int max = 0; // Максимальное количество попыток
+            int count = 0; // Общее количество попыток во всех играх
+            int countGame = 0; // Количество сыгранных игр
+            Random rnd = new Random(); // Создание генератора случайных чисел
+            char answer = 'Y'; // Переменная для ответа пользователя (продолжать игру?)
+            do // Цикл выполняется минимум один раз
             {
-                int couneter = 0;
-                int number = rnd.Next(1, 100);
-                Console.WriteLine("Try guess number?");
-                while (true)
-                {
-                    couneter++;
-                    int userNumber = 0;
-                    Console.WriteLine("Input number from [1;100]");
-                    for (int i = 0; i < 3; i++)
-                    {
-                        if (!int.TryParse(Console.ReadLine(), out userNumber)
-                            || userNumber > 100 || userNumber < 1)
-                            Console.WriteLine("Input number from [1;100]");
-                        else break;
-                        if (i == 2)
-                        {
-                            Console.WriteLine("You are stupid");
-                            return;
-                        }
-                    }
-                    if (userNumber > number)
-                        Console.WriteLine("Your number is great!");
-                    else if (userNumber < number)
-                        Console.WriteLine("Your number is less");
-                    else
-                    {
-                        Console.WriteLine("You are 1Xwin!!!");
-                        if (min==0 || min > couneter) min = couneter;
-                        max = max < couneter ? couneter : max;
-                        count += couneter;
-                        countGame++;
-                        break;
-                    }
-                }
-                Console.WriteLine("Do you want play game?");
-                answer = Convert.ToChar(Console.Read());
-            } while (answer == 'Y');
+                int couneter = PlayGame(rnd, ref min, ref max, ref count, ref countGame);
+                // Вызов игры. Передаём статистику по ссылке (ref), чтобы она изменялась внутри метода
+                Console.WriteLine("Do you want play game?"); // Вопрос о продолжении игры
+                answer = Convert.ToChar(Console.Read()); 
+                // Чтение одного символа из консоли (лучше использовать ReadLine)
+            }
+            while (answer == 'Y'); // Повторяем, пока пользователь вводит 'Y'
+
             Console.WriteLine($"min = {min} max = {max} avg = {(double)count / countGame}");
+            // Вывод статистики: минимум, максимум и среднее количество попыток
+        }
+        static int PlayGame(Random rnd, ref int min, ref int max, ref int count, ref int countGame)
+        // Метод одной игры. Возвращает количество попыток
+        {
+            int couneter = 0; // Счётчик попыток
+            int number = rnd.Next(1, 100); 
+            // Генерация случайного числа от 1 до 99 (100 не включается)
+            Console.WriteLine("Try guess number?"); // Сообщение игроку
+            while (true) // Бесконечный цикл до угадывания числа
+            {
+                couneter++; // Увеличиваем счётчик попыток
+                int userNumber = ReadUserNumber(); 
+                // Чтение числа, введённого пользователем
+                if (userNumber > number) 
+                    Console.WriteLine("Your number is less!"); 
+                    // Подсказка (сообщение написано логически наоборот)
+                else if (userNumber < number) 
+                    Console.WriteLine("Your number is great!"); 
+                    // Подсказка (также перепутан текст)
+
+                else
+                {
+                    Console.WriteLine("You are 1Xwin!!!"); // Сообщение о победе
+                    if (min == 0 || min > couneter) 
+                        min = couneter; 
+                    // Обновляем минимум попыток
+                    max = max < couneter ? couneter : max; 
+                    // Обновляем максимум попыток (тернарный оператор)
+                    count += couneter; 
+                    // Добавляем попытки к общему количеству
+                    countGame++; 
+                    // Увеличиваем количество игр
+                    break; // Выход из цикла после победы
+                }
+            }
+            return couneter; // Возвращаем количество попыток
+        }
+        static int ReadUserNumber() // Метод чтения числа от пользователя
+        {
+            int userNumber = 0; // Переменная для хранения введённого числа
+            Console.WriteLine("Input number from [1;100]"); 
+            // Просим пользователя ввести число от 1 до 100
+            for (int i = 0; i < 3; i++) 
+            // Даём пользователю 3 попытки ввода корректного числа
+            {
+                if (!int.TryParse(Console.ReadLine(), out userNumber)
+                    || userNumber > 100 || userNumber < 1)
+                    // Проверяем: число ли это и входит ли в диапазон
+                    Console.WriteLine("Input number from [1;100]");
+                else 
+                    break; // Если всё корректно — выходим из цикла
+                if (i == 2) // Если третья неудачная попытка
+                {
+                    Console.WriteLine("You are stupid"); 
+                    // Сообщение (некорректное с точки зрения UX)
+                    Environment.Exit(0); 
+                    // Завершение программы
+                }
+            }
+            return userNumber; // Возвращаем введённое число
         }
     }
 }
